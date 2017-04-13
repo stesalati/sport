c907fe19803431c38c4315b6dc2bc6fe619cbc01
stesalati <stef.salati@gmail.com>
2017-04-13 16:52:59 +0200

    Update 3d.py

--- 3d.py
+++ 3d.py
@@ -9,41 +9,92 @@ from osgeo import gdal, osr
 #import OsmApi
 import math
 import os
+import sys
+import gdal_merge as gm
+
+sys.path.append('C:/Users/salatis/AppData/Local/Continuum/Anaconda2/Scripts')
 
 
 
 
-# filename = "elevationdata/srtm_39_04/srtm_39_04.tif"
 
 startingpoint = (44.6478300, 10.9253900)
 track_lat = np.arange(startingpoint[0], startingpoint[0]+0.01, 0.00001).transpose()
 track_lon = np.tile(startingpoint[1], (len(track_lat)))
-margin = 1000
+margin = 400
 
 
 
 
 
+
+
+
+
+"""
+ACTUAL FUNCTION UNDER TEST
+"""
 
 def SRTMTile(lat, lon):
-    xtile = int(np.round((lon - (-180)) / (360/72) + 1))
-    ytile = int(np.round((60 - lat) / (360/72) + 1))
-    name = "srtm_{:02d}_{:02d}".format(xtile, ytile)
-    return name
+    xtile = int(np.trunc((lon - (-180)) / (360/72) + 1))
+    ytile = int(np.trunc((60 - lat) / (360/72) + 1))
+    return (xtile, ytile)
+
+def degrees2metersLongX(latitude, longitudeSpan):
+  # latitude (in degrees) is used to convert a longitude angle to a distance in meters
+  return 2.0*math.pi*earthRadius*math.cos(math.radians(latitude))*longitudeSpan/360.0
+
+def degrees2metersLatY(latitudeSpan):
+  # Convert a latitude angle span to a distance in meters
+  return 2.0*math.pi*earthRadius*latitudeSpan/360.0
+
+def degrees2meters(longitude, latitude):
+  return (degrees2metersLongX(latitude, longitude), degrees2metersLatY(latitude))
 
 earthRadius = 6371000 # Earth radius in meters (yes, it's an approximation) https://en.wikipedia.org/wiki/Earth_radius
+px2deg = 0.0008333333333333334
 
-# Determine central track coordinates and area width
-mylocation = ((np.max(track_lat) + np.min(track_lat))/2, (np.max(track_lon) + np.min(track_lon))/2)
+# Determine the coordinates of the area we are interested in
+center = ((np.max(track_lat) + np.min(track_lat))/2, (np.max(track_lon) + np.min(track_lon))/2)
 span_deg = np.max([np.max(track_lat)-np.min(track_lat), np.max(track_lon)-np.min(track_lon)])
+lat_min = np.min(track_lat) - margin * px2deg
+lat_max = np.max(track_lat) + margin * px2deg
+lon_min = np.min(track_lon) - margin * px2deg
+lon_max = np.max(track_lon) + margin * px2deg
 
-# Choosing the right tile
-tile = SRTMTile(mylocation[0], mylocation[1])
-filename = "elevationdata/{}/{}.tif".format(tile, tile)
-print filename
+# Determine which tiles are necessary
+tile_corner_min = SRTMTile(lat_min, lon_min)
+tile_corner_max = SRTMTile(lat_max, lon_max)
+tiles_lat = range(tile_corner_min[0], tile_corner_max[0]+1)
+tiles_lon = range(tile_corner_min[1], tile_corner_max[1]+1)
 
-if not os.path.isfile(filename):
-    print "Elevation profile for this location ({}) has not been downloaded.".format(tile)
+# For debug
+#tiles_lat = range(38, 39+1)
+#tiles_lon = range(3, 3+1)
+
+print "\nHorizontal tiles: {}".format(tiles_lat)
+print "Vertical tiles: {}\n".format(tiles_lon)
+
+if len(tiles_lat) > 1 or len(tiles_lon) > 1:
+    # More than one tile is needed: generate tile names and merge them
+    gdal_merge_command_list = ['', '-o', 'elevationdata/tmp.tif']
+    for tile_lat in tiles_lat:
+        for tile_lon in tiles_lon:
+            # Generate tile filename and append it to the list
+            tilename = "srtm_{:02d}_{:02d}".format(tile_lat, tile_lon)
+            filename = "elevationdata/{}/{}.tif".format(tilename, tilename)
+            gdal_merge_command_list.append(filename)
+            if not os.path.isfile(filename):
+                print "Elevation profile for this location ({}) has not been downloaded.".format(tilename)
+    # Merge
+    print gdal_merge_command_list
+    gm.main(gdal_merge_command_list)
+    filename = "elevationdata/{}/{}.tif".format(tilename, tilename)
+else:
+    tilename = "srtm_{:02d}_{:02d}".format(tiles_lat[0], tiles_lon[0])
+    filename = "elevationdata/{}/{}.tif".format(tilename, tilename)
+    if not os.path.isfile(filename):
+        print "Elevation profile for this location ({}) has not been downloaded.".format(tilename)
 
 # Read SRTM GeoTiff elevation file 
 ds = gdal.Open(filename)
@@ -55,37 +106,18 @@ tile_lon_max = gt[0] + width*gt[1] + height*gt[2]
 tile_lat_min = gt[3] + width*gt[4] + height*gt[5] 
 tile_lat_max = gt[3]
 tile_ele = ds.GetRasterBand(1)
-span_px = int(np.round(span_deg/gt[1] + margin))
 
-# Check if the location specified is in this tile
-if (mylocation[1] < tile_lon_min) or (mylocation[1] > tile_lon_max) or (mylocation[0] < tile_lat_min) or (mylocation[0] > tile_lat_max):
-    print "Error: the selected location is not covered by the current map"
-mylocation_px = ((mylocation[0]-tile_lat_max)/gt[5], (mylocation[1]-tile_lon_min)/gt[1])
+print "Latitude:  {} <-- {} -- {} --> {}".format(tile_lat_min, lat_min, lat_max, tile_lat_max)
+print "Longitude: {} <-- {} -- {} --> {}\n".format(tile_lon_min, lon_min, lon_max, tile_lon_max)
 
-zone_x_min_tmp = np.round(mylocation_px[1] - span_px * 0.5)
-zone_x_size = span_px
-zone_y_min_tmp = np.round(mylocation_px[0] - span_px * 0.5)
-zone_y_size = span_px
+# Selecting only a zone of the whole map, the one we're interested in plotting
+zone_x_min = int((lon_min - tile_lon_min)/gt[1])
+zone_x_size = int((lon_max - lon_min)/gt[1])
+zone_y_min = int((lat_max - tile_lat_max)/gt[5])
+zone_y_size = int((lat_max - lat_min)/gt[1])
 
-# Trim boundaries
-zone_x_min = zone_x_min_tmp if zone_x_min_tmp>=0 else 0
-zone_y_min = zone_y_min_tmp if zone_y_min_tmp>=0 else 0
-zone_x_size = zone_x_size if (zone_x_min_tmp + zone_x_size)<width else width
-zone_y_size = zone_y_size if (zone_y_min_tmp + zone_y_size)<height else height
-
-# Actual elevation data
+# Read elevation data
 zone_ele = tile_ele.ReadAsArray(zone_x_min, zone_y_min, zone_x_size, zone_y_size).astype(np.float)
-
-def degrees2metersLongX(latitude, longitudeSpan):
-  """ latitude (in degrees) is used to convert a longitude angle to a distance in meters """
-  return 2.0*math.pi*earthRadius*math.cos(math.radians(latitude))*longitudeSpan/360.0
-
-def degrees2metersLatY(latitudeSpan):
-  """ Convert a latitude angle span to a distance in meters """
-  return 2.0*math.pi*earthRadius*latitudeSpan/360.0
-
-def degrees2meters(longitude, latitude):
-  return (degrees2metersLongX(latitude, longitude), degrees2metersLatY(latitude))
 
 # Create X,Y coordinates for zone_ele array (contains Z in meters)
 line_x_deg = np.arange(tile_lon_min+zone_x_min*gt[1], tile_lon_min+(zone_x_min+zone_x_size)*gt[1], gt[1])[0:zone_x_size]
@@ -113,12 +145,12 @@ for i in range(np.size(track_lat, axis=0)):
   track_x_m.append(x)
   track_y_m.append(y)
   zz = zone_ele.transpose()[int(round((track_lon[i] - (tile_lon_min+zone_x_min*gt[1])) / gt[1])), int(round((track_lat[i] - (tile_lat_max+zone_y_min*gt[5])) / gt[5]))]
-  track_z_m.append(zz+30.0) # We display the path 30m over the surface for it to be visible
+  track_z_m.append(zz)
 
 # Display path nodes as spheres
-mlab.points3d(track_x_m, track_y_m, track_z_m, color=(1,0,0), mode='sphere', scale_factor=100)
-# Displaying the line does not work as nodes are not listed in the correct order
-# mlab.plot3d(track_x_m,track_y_m,track_z_m, color=(1,1,0), line_width=15.0) # representation='surface' 'wireframe' 'points'
+# mlab.points3d(track_x_m, track_y_m, track_z_m, color=(1,0,0), mode='sphere', scale_factor=100)
+# Display path as line
+mlab.plot3d(track_x_m, track_y_m, track_z_m, color=(255.0/255.0,102.0/255.0,0), line_width=10.0, tube_radius=50.0)
 
 # Show the 3D map
 mlab.show()
